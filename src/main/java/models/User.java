39dc10181dd2863b726cddaf836951120e9fad67
Dina425 <bazhenovadina@gmail.com>
2025-02-10 18:36:23 +0200

    User

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,20 +1,11 @@
 package models;
 
 public class User {
-    private String firstName;
-    private String lastName;
+
     private String email;
     private String password;
 
-    public User setFirstName(String firstName) {
-        this.firstName = firstName;
-        return this;
-    }
 
-    public User setLastName(String lastName) {
-        this.lastName = lastName;
-        return this;
-    }
 
     public User setEmail(String email) {
         this.email = email;
@@ -28,17 +19,6 @@ public class User {
 
 
 
-
-
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
     public String getEmail() {
         return email;
     }
@@ -50,8 +30,7 @@ public class User {
     @Override
     public String toString() {
         return "User{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
+
                 ", email='" + email + '\'' +
                 ", password='" + password + '\'' +
                 '}';
